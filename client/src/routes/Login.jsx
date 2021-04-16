b499e7411e976ac27ebb4181c658a4a20d972e38
sewn0001 <sewn0001@stud.hs-kl.de>
2021-04-16 23:24:10 +0200

    Revert "login und register"
    This reverts commit 1841d8750cc2060e72a0411f25867878e736d715.

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< Updated upstream
 
 
 const Login = () => {
@@ -13,43 +12,3 @@ const Login = () => {
 };
 
 export default Login;
-=======
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import './route.css';
-
-const Login = () => {
-    return (
-      <Container className="routeContainer">
-        <h1>Willkommen beim Login!</h1>
-          
-      
-        
-          <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="test0001@hs-kl.de" />
-   
-  </Form.Group>
-
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="eingeloggt bleiben" />
-  </Form.Group>
-    <div className="buttonBackground" >
-        <Button href="/home" className="button">Login</Button>
-    </div>
-</Form>
-        
-      </Container>
-
-  
-    );
-  };
-  
-  export default Login;
->>>>>>> Stashed changes
